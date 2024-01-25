420a5ce339c3d358f422b16b07f4d93ff1f5167c
desirobotics <157451513+desirobotics@users.noreply.github.com>
2024-01-25 12:07:25 +0530

    Add files via upload

---
+++ wertigo/wertigo/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "PowerBI Connector",
+
+    'summary': """
+       powerbi connecter
+       """,
+
+    'description': """
+        	Analytics for Odoo made easier
+    """,
+
+    'author': "Odoo Analytics",
+    'category': 'Third Party',
+    'price': 0,
+    'currency': 'USD',
+    'version': '0.1',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'OPL-1',
+    'depends': ['base', ],
+
+    # any module necessary for this one to work correctly
+    'images': ['static/description/banner.gif'],
+   
+    }
